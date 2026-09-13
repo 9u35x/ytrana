7cb510bd3ed73afb21a05a9f1b2672d11c0d67c8
9u35x <yt1272008@gmail.com>
2026-09-13 19:16:07 +0300

    Add path configuration to wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,3 +1,12 @@
+import sys
+import os
+
+path = '/home/270zw/ytrana/backend'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.chdir(path)
+
 from a2wsgi import ASGIMiddleware
 from main import app
 
